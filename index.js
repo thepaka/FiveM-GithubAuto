2072392ea1a14e1b6b0425a6459e4a570fc05a75
ThePaka <thepakamaster@free.fr>
2017-07-19 20:35:48 +0000

    use the server config to get the rcon password and to restart all the AutoStartResources)

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
-const express = require('express')
-const app = express()
+const express = require('express');
 const Rcon = require('rcon');
-const gitpull = require('git-pull')
+const gitpull = require('git-pull');
 const bodyParser = require('body-parser');
 const crypto       = require('crypto')
-    , bufferEq     = require('buffer-equal-constant-time')
+    , bufferEq     = require('buffer-equal-constant-time');
+const yaml = require('js-yaml')
+    , fs = require('fs');
 
-var config = require('./config.json');
+const app = express();
 
 process.env['LANG'] = 'POSIX';
 
-function signBlob (key, blob) {
-    return 'sha1=' + crypto.createHmac('sha1', key).update(blob).digest('hex')
+var config = require('./config.json');
+
+var f;
+if (config.server_cfg_path.startsWith('/')) {
+    f = config.server_cfg_path;
+} else if (config.server_cfg_path != "" ) {
+    f = config.git_pull_path + "/" + config.server_cfg_path;
+}
+
+console.log("using server config file : "+f);
+var srv_config = yaml.load(fs.readFileSync(f));
+
+if (typeof config.rcon_password === "boolean" && config.rcon_password) {
+    config.rcon_password = srv_config.RconPassword;
 }
 
 var conn = new Rcon(config.rcon_host, config.rcon_port, config.rcon_password, { tcp: false, challenge: false } );
@@ -41,10 +54,23 @@ app.use(bodyParser.json({ verify: rawBodySaver }));
 app.use(bodyParser.urlencoded({ verify: rawBodySaver, extended: true }));
 app.use(bodyParser.raw({ verify: rawBodySaver, type: function () { return true } }));
 
-app.get('/refresh', function (req, res) {
+function signBlob (key, blob) {
+    return 'sha1=' + crypto.createHmac('sha1', key).update(blob).digest('hex');
+}
+
+function srv_refresh() {
     conn.send('refresh');
+    for (var ressource of srv_config.AutoStartResources) {
+        conn.send("restart " + ressource);
+    }
+}
+
+app.get('/refresh', function (req, res) {
     res.send('Hello World!');
+    srv_refresh();
 })
+
+
 
 app.post('/webhook', function (req, res) {
     console.log('post /webhook');
@@ -79,9 +105,10 @@ app.post('/webhook', function (req, res) {
                 return;
             }
 
-            conn.send('refresh');
-            // todo : get AutoStartRessources from  config.git_pull_path + ' citmp-server.yml 
-            //       and restart them 
+            srv_refresh();
+
+            // todo : get AutoStartRessources from  config.git_pull_path + ' citmp-server.yml
+            //       and restart them
         }
     });
 })
